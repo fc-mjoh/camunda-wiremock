bb6053f8a47c129468e9cb333c11fbcc2dae63ae
m.johannsen <markus.johannsen@reply.de>
2025-04-11 14:26:43 +0200

    first commit

---
+++ src/test/java/io/mjoh/wiremock/camunda/CamundaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mjoh.wiremock.camunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
